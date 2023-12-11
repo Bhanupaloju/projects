f95c9784db7feca3209749d1fa205379b9782233
Bhanupaloju <118360694+Bhanupaloju@users.noreply.github.com>
2023-12-11 10:40:16 +0530

    Delete instaclone directory

--- instaclone/one.js
+++
@@ -1,8 +0,0 @@
-let image = document.querySelector('img.img');
-image.addEventListener('click', function(e) {
-    image.requestFullscreen();
-})
-
-image.addEventListener('dblckick', function(e) {
-    document.exitFullscreen();
-})
