1c6f48db7875af3bfeaaa88a8bd81bf5ae64c084
Bhanupaloju <bhanupaloju5@gmail.com>
2024-05-04 12:05:20 +0530

    food delivery application using react js

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order Your favourite food here</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+          a rem blanditiis quas beatae? At, adipisci? Corrupti provident vitae
+          nemo dolore quo, fuga voluptatum? Rerum voluptate ex doloremque nulla
+          iusto.
+        </p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
